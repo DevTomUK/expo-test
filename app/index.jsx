351004eb351556fad06e4a015aa304b5c646e573
DevTomUK <tomghdev@gmail.com>
2024-05-16 16:34:07 +0100

    firebase read/write working

---
+++ app/index.jsx
@@ -0,0 +1,110 @@
+import { useEffect, useState } from "react";
+import { TextInput, TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import {db} from './config'
+import { getDatabase, ref, set, get, child } from "firebase/database";
+
+export default function Index() {
+
+  const [createNum, setCreateNum] = useState(0)
+
+  function create () {
+
+    const newNum = (Math.random()*100).toFixed(0)
+    setCreateNum(newNum)
+    // .then(()=>{
+    //   alert("submitted")
+    // })
+    // .catch((error)=>{
+    //   alert(error)
+    // })
+  }
+
+      useEffect(()=>{
+        set(ref(db, 'test/' + createNum), {
+          testRow: 'test string',
+      }, [create])
+      })
+
+  function readClick () {
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `test/`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+      }
+
+  const [countAmount, setCountAmount] = useState(0)
+  const [theme, setTheme] = useState('light')
+  const [BGCol, setBGCol] = useState('#fff')
+  const [textCol, setTextCol] = useState('#333')
+
+  function handleClick(){
+    setCountAmount(curr => curr + 1)
+  }
+
+  function handleTheme() {
+    if (theme === 'light') {
+      setTheme('dark')
+      setBGCol('#333')
+      setTextCol('#fff')
+    }
+    if (theme === 'dark') {
+      setTheme('light')
+      setBGCol('#fff')
+      setTextCol('#333')
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 24,
+      backgroundColor: BGCol,
+    },
+    title: {
+      marginTop: 40,
+      color: textCol,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      fontWeight: 'bold',
+      fontSize: 20,
+    },
+    clicker: {
+      marginTop: 20,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+    clickerText: {
+      fontSize: 100,
+      color: textCol,
+    },
+    countAmount: {
+      fontSize: 20,
+    },
+    textInput: {
+      borderColor: 'gray',
+      borderWidth: 0.2
+    }
+  })
+
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.title}>Test!</Text>
+      <Text style={styles.countAmount}>{countAmount}</Text>
+      <Text style={styles.countAmount}>{createNum}</Text>
+      <TextInput style={styles.textInput}>Type here</TextInput>
+      <TouchableOpacity style={styles.clicker} onPress={create}><Text style={styles.clickerText}>Create</Text></TouchableOpacity>
+      <TouchableOpacity style={styles.clicker} onPress={readClick}><Text style={styles.clickerText}>Read</Text></TouchableOpacity>
+      <TouchableOpacity style={styles.clicker} onPress={handleTheme}><Text style={styles.clickerText}>Theme</Text></TouchableOpacity>
+    </View>
+  );
+
+
+}
